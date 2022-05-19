8b54c8f57767a41828d57233126c107292737763
takumi <y.takumi4@gmail.com>
2022-05-20 02:43:36 +0900

    デバックモードを変更

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-DEBUG = True
+DEBUG = False
 
 
 
