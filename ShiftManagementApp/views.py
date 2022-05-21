d9ad3a6fab7d3d3892636a54aaef2ac484d91178
takumi <y.takumi4@gmail.com>
2022-05-21 17:44:38 +0900

    exceptの時にもres_codeを返すように修正

--- ShiftManagementApp/views.py
+++ ShiftManagementApp/views.py
@@ -426,6 +426,9 @@ def editshift_ajax_delete_shiftdata(request):
                 'res_code':True
             })
         except Exception as e:
+            response.append({
+                'res_code':False
+            })
             print(e)
         return JsonResponse(response,safe=False)
 
