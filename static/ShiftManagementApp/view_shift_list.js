9543d253f385b94c4c6093aa1c1b3e0cb736e610
takumi <y.takumi4@gmail.com>
2022-07-01 02:18:30 +0900

    ドロップダウンリストから選択して表示するようにした

--- static/ShiftManagementApp/view_shift_list.js
+++ static/ShiftManagementApp/view_shift_list.js
@@ -1,29 +1,44 @@
 /*ページ(DOM)読み込み後に実行*/
-window.onload = function(){
+//window.onload = 
+function view_shift_lists(){
+    const tableEle = document.getElementById('data-table');
+
+    /**表が下に増えていくのを防ぐためクリックするたび表を削除する */
+    tableEle.innerHTML = "";
+
+    axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
+    axios.defaults.xsrfCookieName = "csrftoken"
 
     axios
-        .get('/shift-list-ajax/')
+        .post('/shift-list-ajax/',{
+            "selected_month": $('#selected_month').val()
+        })
         .then((res)=>{
             console.log(res);
-            const end_date = new Date(2022,6,31);
-            let shift_list_date = document.getElementById('shift_list_date');
-            const dt = new Date(2022,6,1)
-            const dt_last_date = new Date(dt.getFullYear(),dt.getMonth()+1,0);
-            //console.log(dt_last_date)
+            let thead_tr = document.createElement('tr');
+            const selected_month = $()
+            const date_for_display = new Date(2022,6,1)
+            const dt_last_date = new Date(date_for_display.getFullYear(),date_for_display.getMonth()+1,0);
+            console.log(dt_last_date)
             for(let i=1;i<=dt_last_date.getDate();i++){
                 let th = document.createElement('th');
-                dt_month = dt.getMonth();
-                dt_date = dt.getDate();
+                dt_month = date_for_display.getMonth()+1;
+                dt_date = date_for_display.getDate();
                 th.innerHTML = dt_month + "/" + dt_date;
-                shift_list_date.appendChild(th);
-                dt.setDate(dt.getDate()+1);
+                thead_tr.appendChild(th);
+                date_for_display.setDate(date_for_display.getDate()+1);
             }
+
+            const thead = document.createElement('thead');
+            thead.appendChild(thead_tr);
+
+            tableEle.appendChild(thead);
             //console.log(end_date.getDate());
             //tableEle.appendChild(shift_list_date);
-            tbody = document.getElementById('tbody')
+            tbody = document.createElement('tbody')
 
             /**個人ごとにループを回す */
-            for(let i=0;i<res.data.shift_lists.length;i++){
+            for(let person_i=0;person_i<res.data.shift_lists.length;person_i++){
                 let tr = document.createElement('tr');
                 let th = document.createElement('th');
                 let td = document.createElement('td');
@@ -31,28 +46,26 @@ window.onload = function(){
                 const start_date = new Date(2022,6,1,9,0,0);
                 const search_date = start_date;
 
-                th.innerHTML = res.data.shift_lists[i].username;
+                th.innerHTML = res.data.shift_lists[person_i].username;
                 tr.appendChild(th)
 
                 let count = 0;
                 let break_counter = 0;
 
                 //console.log(start_date);
-                //dt = new Date(res.data.shift_lists[i].shift_list[j].date)
+                //dt = new Date(res.data.shift_lists[person_i].shift_list[j].date)
                 //console.log(dt)
+
+                /**検索する日付と、その月末が並ぶまでループする*/
                 while(search_date.getDate()<=dt_last_date.getDate()){
                     let td = document.createElement('td'); //毎回定義しないと同じエレメントに上書きされる
-                    //console.log(new Date(res.data.shift_lists[i].shift_list[count].date));
-                    //console.log("search_date:"+search_date);
-                    console.log(search_date.getDate());
-                    console.log(end_date.getDate());
 
-                    if(count == res.data.shift_lists[i].shift_list.length){
+                    if(count == res.data.shift_lists[person_i].shift_list.length){
                         td.innerHTML = ""
                     }
-                    else if(new Date(res.data.shift_lists[i].shift_list[count].date).getTime() == search_date.getTime()){
-                        start_date_object = new Date(res.data.shift_lists[i].shift_list[count].start);
-                        end_date_object = new Date(res.data.shift_lists[i].shift_list[count].end);
+                    else if(new Date(res.data.shift_lists[person_i].shift_list[count].date).getTime() == search_date.getTime()){
+                        start_date_object = new Date(res.data.shift_lists[person_i].shift_list[count].start);
+                        end_date_object = new Date(res.data.shift_lists[person_i].shift_list[count].end);
 
                         start_hour_str = start_date_object.getHours();
                         start_minutes_str = start_date_object.getMinutes();
@@ -90,6 +103,7 @@ window.onload = function(){
                 tr.appendChild(fragment);
                 console.log(tr);
                 tbody.appendChild(tr);
+                tableEle.appendChild(tbody);
   
             }
         })
@@ -97,3 +111,6 @@ window.onload = function(){
             console.log(res);
         })
     }
+
+
+    
