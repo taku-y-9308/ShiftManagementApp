b885993615931813f575f628f40ec5b9142f74a1
takumi <y.takumi4@gmail.com>
2022-07-11 22:13:51 +0900

    一般ユーザーがシフト削除できない問題を解消 #21

--- ShiftManagementApp/views.py
+++ ShiftManagementApp/views.py
@@ -546,7 +546,8 @@ def editshift_ajax_delete_shiftdata(request):
         削除リクエストの判定
         編集可能期間もしくは、編集モードの時に削除リクエストを受け付ける
         """
-        if (Judge_editable(datas['start']) == True or request.user.is_edit_mode == True):
+        for_judge_date = f"{datas['date']}T{datas['start']}"
+        if (Judge_editable(for_judge_date) == True or request.user.is_edit_mode == True):
             #getは対象が存在しないと例外を返すため念の為try文にしている
             try:
                 Shift.objects.get(id=datas['id']).delete()
