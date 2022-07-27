743a4e3ffc98160fde979c3d48761f897b5a1d15
takumi <y.takumi4@gmail.com>
2022-07-27 15:31:34 +0900

    テストDBの設定を追加

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dtvn6smssuuna',
-        'USER': '',
+        'USER': os.environ['USER'],
         'PASSWORD': os.environ['PASSWORD'],
         'HOST': 'localhost',
         'PORT': '5432',
