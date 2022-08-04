8ddc83e9f97ceccbba93bc62133ce74bb715167f
takumi <y.takumi4@gmail.com>
2022-08-04 17:52:13 +0900

    管理サイトから登録LINEユーザを確認できるように変更 #40

--- ShiftManagementApp/admin.py
+++ ShiftManagementApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from ShiftManagementApp.models import User, Shift,Contact
+from ShiftManagementApp.models import User, Shift,Contact,LINE_USER_ID
 
 admin.site.register(User)
 admin.site.register(Shift)
 admin.site.register(Contact)
+admin.site.register(LINE_USER_ID)
