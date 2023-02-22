4245f2bab04b4bfd069eb67643c94b3d72ce5d41
takumi <y.takumi4@gmail.com>
2023-02-22 23:53:51 +0900

    管理画面の一覧表示に表示する情報を追加

--- ShiftManagementApp/admin.py
+++ ShiftManagementApp/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from ShiftManagementApp.models import User, Shift,Shift_Archive,Contact,LINE_USER_ID,Deadline
 
-admin.site.register(User)
-admin.site.register(Shift_Archive)
-admin.site.register(Shift)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','shop_id','username','email','is_edit_mode','is_staff','is_active']
+
+class ShiftAdmin(admin.ModelAdmin):
+    list_display = ['user','date','begin','finish','position','publish']
+
+class LineAdmin(admin.ModelAdmin):
+    list_display = ['user','line_user_id']
+
+class DeadLineAdmin(admin.ModelAdmin):
+    list_display = ['shop_id','deadline']
+admin.site.register(User,UserAdmin)
+admin.site.register(Shift_Archive,ShiftAdmin)
+admin.site.register(Shift,ShiftAdmin)
 admin.site.register(Contact)
-admin.site.register(LINE_USER_ID)
-admin.site.register(Deadline)
+admin.site.register(LINE_USER_ID,LineAdmin)
+admin.site.register(Deadline,DeadLineAdmin)
